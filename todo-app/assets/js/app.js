0b5619b7e4edf5df0e5cc60c966bb127087a2811
mohamed96321 <atefm.me499@gmail.com>
2023-11-16 19:21:13 +0200

    Fix drag drop styling issue when element moved

--- todo-app/assets/js/app.js
+++ todo-app/assets/js/app.js
@@ -145,21 +145,25 @@ class TaskListView {
       if (targetElement === draggingElement) {
         return;
       }
-      const rect = targetElement.getBoundingClientRect();
-      const offset = rect.y + rect.height / 2;
-      if (event.clientY - offset > 0) {
-        targetElement.style['border-bottom'] = 'solid 1px #000';
-        targetElement.style['border-top'] = '';
-      } else {
-        targetElement.style['border-bottom'] = '';
-        targetElement.style['border-top'] = 'solid 1px #000';
+      if (targetElement) {
+        const rect = targetElement.getBoundingClientRect();
+        const offset = rect.y + rect.height / 2;
+        if (event.clientY - offset > 0) {
+          targetElement.style['border-bottom'] = 'solid 1px #000';
+          targetElement.style['border-top'] = '';
+        } else {
+          targetElement.style['border-bottom'] = '';
+          targetElement.style['border-top'] = 'solid 1px #000';
+        }
       }
     });
 
     this.taskListElem.addEventListener('dragleave', (event) => {
       const targetElement = event.target.closest('.task-item');
-      targetElement.style['border-bottom'] = '';
-      targetElement.style['border-top'] = '';
+      if (targetElement) {
+        targetElement.style['border-bottom'] = '';
+        targetElement.style['border-top'] = '';
+      }
     });
 
     this.taskListElem.addEventListener('drop', (event) => {
@@ -217,7 +221,7 @@ class TaskListView {
     paginationElem.innerHTML = `
       <div class="central-div d-flex justify-content-center">
         <button class="prev-btn btn btn-primary me-2 ${
-        this.currentPage === 1 ? 'disabled' : ''
+          this.currentPage === 1 ? 'disabled' : ''
         }">Previous</button>
         <button class="next-btn btn btn-primary ${
           endIndex >= this.taskList.tasks.length ? 'disabled' : ''
